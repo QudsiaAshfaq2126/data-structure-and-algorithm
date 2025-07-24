0ec86d4156597ddf664f597414f3ebd63af8670a
QudsiaAshfaq2126 <qudsiaashfaq2621@gmail.com>
2025-07-24 17:22:09 +0500

    Add files via upload

---
+++ Selection_sort/task_1.py
@@ -0,0 +1,24 @@
+def multi_level_sort(data, sorting_order):
+    return sorted(data, key=lambda x: tuple(x[key] for key in sorting_order))
+
+data = [
+    {'First Name': 'Raj', 'Last Name': 'Nayyar'},
+    {'First Name': 'Suraj', 'Last Name': 'Sharma'},
+    {'First Name': 'Karan', 'Last Name': 'Kumar'},
+    {'First Name': 'Jade', 'Last Name': 'Canary'},
+    {'First Name': 'Raj', 'Last Name': 'Thakur'},
+    {'First Name': 'Raj', 'Last Name': 'Sharma'},
+    {'First Name': 'Kiran', 'Last Name': 'Kamla'},
+    {'First Name': 'Armaan', 'Last Name': 'Kumar'},
+    {'First Name': 'Jaya', 'Last Name': 'Sharma'},
+    {'First Name': 'Ingrid', 'Last Name': 'Galore'},
+    {'First Name': 'Jaya', 'Last Name': 'Seth'},
+    {'First Name': 'Armaan', 'Last Name': 'Dadra'},
+    {'First Name': 'Ingrid', 'Last Name': 'Maverick'},
+    {'First Name': 'Aahana', 'Last Name': 'Arora'}
+]
+
+sorted_data = multi_level_sort(data, ['First Name', 'Last Name'])
+
+for entry in sorted_data:
+    print(entry)
